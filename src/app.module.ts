68c247b32af24d2650689ed2890cf45446c78aab
Arsen <arsen@MacBook-Air-Arsen.local>
2023-03-07 16:48:02 +0300

    env, clear start app

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' })],
+  providers: [],
+})
+export class AppModule {
+
+}
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = await app.get(ConfigService)
+  const port = config.get('API_PORT') || 3000
+  await app.listen(port, () => {
+    console.log(`App start on: ${port}`)
+  });
+}
+bootstrap();
